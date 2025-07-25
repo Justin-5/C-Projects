804a011f199b93916d9c42670be0ce245b5e9de0
Justin-5 <justinaroza5@gmail.com>
2025-07-25 22:07:11 +0530

    Sudoku Solver Project

---
+++ C_Sudoku_Solver/include/sudoku.h
@@ -0,0 +1,29 @@
+#ifndef SUDOKU_H
+#define SUDOKU_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+extern char POSSIBLE;
+extern int SIZE_ROWS;
+extern int SIZE_COLUMNS;
+
+typedef struct Box
+{
+    struct Box * next;
+} Box;
+
+typedef struct Square
+{
+    int number;
+    char code;
+    Box * box;
+    int row;
+    int column;
+} Square;
+
+int ** createPuzzle();
+void printPuzzle(int ** puzzle);
+Square *** setUpPuzzle(int ** puzzle);
+
+#endif
